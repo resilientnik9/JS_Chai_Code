76adc5fb98197e769c324cf03e7a499f24923d49
Nikhil Anand <nikanand0118@gmail.com>
2024-05-19 17:25:37 +0000

    contunue learning

---
+++ 01_Basics/conversiondatatypes.js
@@ -0,0 +1,8 @@
+let yourName = "Nikhil Anand";
+console.log(yourName);
+console.log(typeof yourName);
+
+console.log(Number(yourName));
+
+
+// jyada kch hai nhi is lec me krne ke liye, just for the sake of creating a module for this lec, have created this file and did a small conversion from string to number although it's not correct.
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,29 @@
-console.log("Nikhil Anand!");
+// console.log("Nikhil Anand!");
+
+// const id1 = Symbol('18');
+// const id2 = Symbol('18');
+
+// console.log(id1, id2);
+// console.log(id1 === id2);
+// console.log(id1 == id2);
+
+const fullName = "Nikhil_Anand"; // datatype for this is string
+const yourName = new String("Anand Nikhil"); // data type for this is an object, it return an object having different methods of string datatype
+// console.log(fullName, fullName[0], typeof fullName);
+// console.log(yourName, yourName[0], typeof yourName);
+
+// console.log(fullName.substring(1, 4));
+
+
+// console.log(yourName.slice(-4, -1));
+
+const age = 18.199;
+// console.log(typeof age);
+// console.log(typeof age.toFixed(1));
+// hitesh-hc
+
+// slice and substring both works same, takes the pos from where we have to start, second parameter is the final pos (excluded)
+
+
+const randomNum = Math.trunc(Math.random() * 6);
+console.log(randomNum);
